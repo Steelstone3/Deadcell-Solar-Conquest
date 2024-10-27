a8367d7f3ddea06e3021b7bf98f8652ebf9cdde5
Skippy <steelstone3@gmail.com>
2024-10-27 11:04:31 +0000

    version-27-10-24

--- src/components/user_interface/selection.rs
+++ src/components/user_interface/selection.rs
@@ -1,4 +1,4 @@
-use crate::assets::user_interface::team::TeamSelectionSprite;
+use crate::assets::user_interface::selection_indicator::SelectionIndicator;
 use bevy::prelude::Component;
 
 #[derive(Component, Clone, Copy)]
@@ -6,11 +6,13 @@ pub struct Selectable;
 
 #[derive(Component, Clone, Copy)]
 pub struct SelectedSprite {
-    pub sprite_path: TeamSelectionSprite,
+    pub sprite_path: SelectionIndicator,
 }
 
 impl SelectedSprite {
-    pub fn new(sprite_path: TeamSelectionSprite) -> Self {
-        Self { sprite_path }
+    pub fn new(team_selection_sprite: SelectionIndicator) -> Self {
+        Self {
+            sprite_path: team_selection_sprite,
+        }
     }
 }
