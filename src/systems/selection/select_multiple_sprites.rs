dbf771ffc591fc34e58308cfe7a02fbe90f32909
Skippy <steelstone3@gmail.com>
2024-10-28 00:16:40 +0000

    version-28-10-24

--- src/systems/selection/select_multiple_sprites.rs
+++ src/systems/selection/select_multiple_sprites.rs
@@ -1,16 +1,20 @@
 use bevy::{
     math::{Rect, Vec2},
-    prelude::{EventReader, Query},
+    prelude::{Commands, EventReader, EventWriter, Query, ResMut},
 };
 
 use crate::{
-    events::selection_events::SelectionAreaEvent, queries::user_interface_queries::SelectableQuery,
+    components::user_interface::{selection::SelectedSprite, tracking::Tracking}, events::{selection_events::SelectionAreaEvent, spawn_sprite_event::{SpawnSprite, SpawnSpriteEvent}}, queries::user_interface_queries::{SelectableQuery, TypeCheckQuery}, resources::spawn_menu_selection::SpawnMenuSelection, systems::user_interface::interactions::spawn_selection::SpawnSelection
 };
 
 // TODO finish this off
 pub fn select_multiple_sprites(
+    mut commands: Commands,
     mut selection_area_reader: EventReader<SelectionAreaEvent>,
     selectable_queries: Query<SelectableQuery>,
+    type_check_query: Query<TypeCheckQuery>,
+    mut spawn_menu_selection: ResMut<SpawnMenuSelection>,
+    mut spawn_sprite_writer: EventWriter<SpawnSpriteEvent>,
 ) {
     let Some(selection_area) = selection_area_reader.read().last() else {
         return;
@@ -38,6 +42,30 @@ pub fn select_multiple_sprites(
         );
 
         if area.contains(selectable_location) {
+            // TODO For starships only
+            SpawnMenuSelection::default_selection(&mut spawn_menu_selection);
+            spawn_menu_selection.selection = SpawnSelection::MultipleSelections;
+
+
+            let selection = SelectedSprite::default();
+            let selection_entity = commands
+            .spawn(selection)
+            .insert(Tracking {
+                entity_to_follow: selectable.entity,
+            })
+            .id();
+
+            let Some(size) = selectable.sprite.custom_size else {
+                return;
+            };
+
+            spawn_sprite_writer.send(SpawnSpriteEvent::spawn_sprite(SpawnSprite {
+                sprite_path: selection.sprite_path.to_string(),
+                size,
+                transform: *selectable.transform,
+                entity: selection_entity,
+            }));
+        
             // TODO select these starships
             // TODO assign the SpawnSelection resource to MultipleSelections
         }
