fd7f2a4c95adba403f8d438f081c630247cdc707
Skippy <steelstone3@gmail.com>
2026-08-15 00:17:18 +0100

    version-15-08-26

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,22 +1,19 @@
 # Ubuntu
-sudo apt install --fix-missing g++ pkg-config lld clang
-doas apt install --fix-missing g++ pkg-config lld clang
-sudo apt install --fix-missing libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
-doas apt install --fix-missing libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
+apt install --fix-missing g++ pkg-config lld clang
+apt install --fix-missing libx11-dev libasound2-dev libudev-dev libxkbcommon-x11-0 librust-alsa-sys-dev librust-libudev-sys-dev libwayland-dev libxkbcommon-dev
 
 # Fedora
 
 ## dnf5
-sudo dnf5 install gcc-c++ lld clang
-doas dnf5 install gcc-c++ lld clang
-sudo dnf5 install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
-doas dnf5 install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
+dnf5 install gcc-c++ lld clang
+dnf5 install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 
 ## dnf4
-sudo dnf install gcc-c++ lld clang
-doas dnf install gcc-c++ lld clang
-sudo dnf install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
-doas dnf install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
+dnf install gcc-c++ lld clang
+dnf install libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
+
+## rpm-ostree
+rpm-ostree install gcc-c++ lld clang libX11-devel alsa-lib-devel systemd-devel wayland-devel libxkbcommon-devel
 
 # cargo
 cargo install cargo make
