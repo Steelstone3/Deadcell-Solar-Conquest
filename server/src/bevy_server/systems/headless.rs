a16c7b1f33ba0e4d62c51e5d50f98fb8dce3f34e
Skippy <steelstone3@gmail.com>
2026-08-16 20:56:21 +0100

    version-16-08-26

--- server/src/bevy_server/systems/headless.rs
+++ server/src/bevy_server/systems/headless.rs
@@ -3,9 +3,6 @@ use bevy::prelude::{Res, Time};
 pub fn log_headless_bevy_status(time: Res<Time>) {
     let secs = time.elapsed_secs_f64();
     if secs.fract() < 0.0001 {
-        tracing::info!(
-            "bevy heartbeat: {:.1}s elapsed | server",
-            secs
-        );
+        tracing::info!("bevy heartbeat: {:.1}s elapsed | server", secs);
     }
 }
