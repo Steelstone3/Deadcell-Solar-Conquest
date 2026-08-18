3bb9eea0c7901d8e3bb91d3801c5400f1e02a4df
Skippy <steelstone3@gmail.com>
2026-08-19 00:31:46 +0100

    version-19-08-26

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,33 +4,51 @@ use bevy_renet::{
     netcode::{NetcodeServerPlugin, NetcodeServerTransport, ServerAuthentication, ServerConfig},
     renet::ConnectionConfig,
 };
-use std::{net::UdpSocket, time::SystemTime};
+use std::{io::Error, net::UdpSocket, time::SystemTime};
 
 fn main() {
+    let transport = match create_server_configuration() {
+        Ok(transport) => transport,
+        Err(_) => return,
+    };
+    let server = RenetServer::new(ConnectionConfig::default());
+
     let mut app = App::new();
     app.add_plugins(RenetServerPlugin);
-
-    let server = RenetServer::new(ConnectionConfig::default());
     app.insert_resource(server);
 
     // Transport layer setup
     app.add_plugins(NetcodeServerPlugin);
-    let transport = create_server_configuration();
     app.insert_resource(transport);
 }
 
-fn create_server_configuration() -> NetcodeServerTransport {
-    let server_addr = "127.0.0.1:5000".parse().unwrap();
-    let socket = UdpSocket::bind(server_addr).unwrap();
+fn create_server_configuration() -> Result<NetcodeServerTransport, Error> {
+    let server_address = match "127.0.0.1:5000".parse() {
+        Ok(server_address) => server_address,
+        Err(_) => {
+            return Err(Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Invalid server address",
+            ));
+        }
+    };
+
+    let socket = UdpSocket::bind(server_address).map_err(|e| {
+        Error::new(
+            std::io::ErrorKind::AddrInUse,
+            format!("Failed to bind socket: {}", e),
+        )
+    })?;
     let server_config = ServerConfig {
         current_time: SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap(),
+            .unwrap_or_default(),
         max_clients: 64,
         protocol_id: 0,
-        public_addresses: vec![server_addr],
+        public_addresses: vec![server_address],
         authentication: ServerAuthentication::Unsecure,
     };
 
-    NetcodeServerTransport::new(server_config, socket).unwrap()
+    NetcodeServerTransport::new(server_config, socket)
+        .map_err(|e| Error::other(format!("Failed to create server transport: {}", e)))
 }
