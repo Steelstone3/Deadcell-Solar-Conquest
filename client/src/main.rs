ee046444133a406fe454b7a5695287b87fcf9335
Skippy <steelstone3@gmail.com>
2026-08-19 04:16:24 +0100

    version-19-08-26

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,8 +5,16 @@ use crate::plugins::{
     user_interface_plugin::UserInterfacePlugin,
 };
 use bevy::{prelude::*, window::WindowResolution};
-use bevy_egui::EguiPlugin;
-use bevy_renet::{RenetClientPlugin, netcode::NetcodeClientPlugin};
+use std::{net::UdpSocket, time::SystemTime};
+// use bevy_egui::EguiPlugin;
+use bevy_renet::{
+    RenetClient, RenetClientPlugin,
+    netcode::{
+        ClientAuthentication, NetcodeClientPlugin, NetcodeClientTransport, NetcodeErrorEvent,
+    },
+    renet::ConnectionConfig,
+};
+use deadcell_solar_conquest_shared::resources::lobby::{ClientChannel, ServerChannel};
 
 mod components;
 mod events;
@@ -39,7 +47,6 @@ fn main() {
             }),
         // EguiPlugin::default(),
         RenetClientPlugin,
-        NetcodeClientPlugin,
         ClientStartPlugin,
         ClientUpdatePlugin,
         EventsPlugin,
@@ -49,5 +56,54 @@ fn main() {
         RunningPlugin,
     ));
 
+    // pub const PROTOCOL_ID: u64 = 7;
+
+    app.add_plugins(NetcodeClientPlugin);
+
+    app.configure_sets(Update, Connected.run_if(bevy_renet::client_connected));
+
+    let client = RenetClient::new(connection_config());
+
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let current_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+    let client_id = current_time.as_millis() as u64;
+    let authentication = ClientAuthentication::Unsecure {
+        client_id,
+        protocol_id: 7,
+        server_addr,
+        user_data: None,
+    };
+
+    let transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+    app.insert_resource(client);
+    app.insert_resource(transport);
+    app.insert_resource(CurrentClientId(client_id));
+
+    // If any error is found we just panic
+    // #[allow(clippy::never_loop)]
+    // fn panic_on_error(error: On<NetcodeErrorEvent>) {
+    //     panic!("{}", *error);
+    // }
+
+    // app.add_observer(panic_on_error);
+
     app.run();
+}
+
+#[derive(SystemSet, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Connected;
+
+#[derive(Debug, Resource)]
+struct CurrentClientId(u64);
+
+pub fn connection_config() -> ConnectionConfig {
+    ConnectionConfig {
+        available_bytes_per_tick: 1024 * 1024,
+        client_channels_config: ClientChannel::channels_config(),
+        server_channels_config: ServerChannel::channels_config(),
+    }
 }
--- shared/src/resources/lobby.rs
+++ shared/src/resources/lobby.rs
@@ -1,8 +1,76 @@
 use bevy::ecs::{entity::Entity, resource::Resource};
-use bevy_renet::renet::ClientId;
-use std::collections::HashMap;
+use bevy_renet::renet::{ChannelConfig, ClientId, SendType};
+use std::{collections::HashMap, time::Duration};
 
 #[derive(Debug, Default, Resource)]
 pub struct ServerLobby {
     pub players: HashMap<ClientId, Entity>,
+}
+
+pub enum ClientChannel {
+    Input,
+    Command,
+}
+
+impl From<ClientChannel> for u8 {
+    fn from(channel_id: ClientChannel) -> Self {
+        match channel_id {
+            ClientChannel::Command => 0,
+            ClientChannel::Input => 1,
+        }
+    }
+}
+
+impl ClientChannel {
+    pub fn channels_config() -> Vec<ChannelConfig> {
+        vec![
+            ChannelConfig {
+                channel_id: Self::Input.into(),
+                max_memory_usage_bytes: 5 * 1024 * 1024,
+                send_type: SendType::ReliableOrdered {
+                    resend_time: Duration::ZERO,
+                },
+            },
+            ChannelConfig {
+                channel_id: Self::Command.into(),
+                max_memory_usage_bytes: 5 * 1024 * 1024,
+                send_type: SendType::ReliableOrdered {
+                    resend_time: Duration::ZERO,
+                },
+            },
+        ]
+    }
+}
+
+pub enum ServerChannel {
+    ServerMessages,
+    NetworkedEntities,
+}
+
+impl From<ServerChannel> for u8 {
+    fn from(channel_id: ServerChannel) -> Self {
+        match channel_id {
+            ServerChannel::NetworkedEntities => 0,
+            ServerChannel::ServerMessages => 1,
+        }
+    }
+}
+
+impl ServerChannel {
+    pub fn channels_config() -> Vec<ChannelConfig> {
+        vec![
+            ChannelConfig {
+                channel_id: Self::NetworkedEntities.into(),
+                max_memory_usage_bytes: 10 * 1024 * 1024,
+                send_type: SendType::Unreliable,
+            },
+            ChannelConfig {
+                channel_id: Self::ServerMessages.into(),
+                max_memory_usage_bytes: 10 * 1024 * 1024,
+                send_type: SendType::ReliableOrdered {
+                    resend_time: Duration::from_millis(200),
+                },
+            },
+        ]
+    }
 }
