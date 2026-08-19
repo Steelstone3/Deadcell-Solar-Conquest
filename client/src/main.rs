72684b598c2ad4eb3f48c35f517265974ab5ac97
Skippy <steelstone3@gmail.com>
2026-08-19 03:50:34 +0100

    version-19-08-26

--- client/src/main.rs
+++ client/src/main.rs
@@ -37,7 +37,7 @@ fn main() {
                 }),
                 ..Default::default()
             }),
-        EguiPlugin::default(),
+        // EguiPlugin::default(),
         RenetClientPlugin,
         NetcodeClientPlugin,
         ClientStartPlugin,
--- client/src/plugins/client_start_plugin.rs
+++ client/src/plugins/client_start_plugin.rs
@@ -1,7 +1,11 @@
-use bevy::app::Plugin;
+use bevy::app::{Plugin, Startup};
+
+use crate::systems::camera::spawn_camera::spawn_camera;
 
 pub struct ClientStartPlugin;
 
 impl Plugin for ClientStartPlugin {
-    fn build(&self, _: &mut bevy::app::App) {}
+    fn build(&self, app: &mut bevy::app::App) {
+        app.add_systems(Startup, spawn_camera);
+    }
 }
